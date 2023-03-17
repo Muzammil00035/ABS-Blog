d681a524d05291c56b9a88c2bdd927f27515f206
Muhammad Muzammil <muhammad.muzammil@evolutionwing.com>
2023-03-17 23:19:26 +0500

    Ip addition

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Models\RecentActivity;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Stevebauman\Location\Facades\Location;
@@ -30,21 +29,22 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request)
     {
-        // $ip = $request->ip();
-        $ip = '48.188.144.248';
+        $ip = $request->ip();
+        // $ip = '48.188.144.248';
         $currentUserInfo = Location::get($ip);
-        // if ($currentUserInfo) {
-        //     $request
-        // }
+
         $request->authenticate();
 
         if ($request->session()->regenerate()) {
-            $recent = new RecentActivity();
-            $recent->user_id = $request->user()->id;
-            $recent->ip = $ip;
-            $recent->country = $currentUserInfo->countryName;
+            if (!empty($currentUserInfo) || $currentUserInfo) {
 
-            $recent->save();
+                $recent = new RecentActivity();
+                $recent->user_id = $request->user()->id;
+                $recent->ip = $ip;
+                $recent->country = $currentUserInfo->countryName; 
+
+                $recent->save();
+            }
 
         }
 
